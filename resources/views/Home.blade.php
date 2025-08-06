663da04bcfdefaf2f5c5d3aef5779e9761b56bb3
ahrafi16 <anjumhossainrafi16@gmail.com>
2025-08-06 12:34:34 +0600

    I agree

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,15 +1,23 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+
 </head>
+
 <body>
 
-    <h1>Hello</h1>
+    <!-- banner -->
+    <section class="s">
+        <h1 class="text-3xl text-red-600 font-bold">FIND YOUR PERFECT PARKING SPOT</h1>
+        <p class="text-6xl text-red-900">fhgvkh</p>
+    </section>
 
 </body>
+
 </html>
